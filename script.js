141c646cae9d8d9b2e40db4c0ad26457b957561d
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-09-12 13:21:56 -0300

    Exercicios corrigidos dia 12-09-2021

---
+++ script.js
@@ -0,0 +1,42 @@
+// let numeroUsuario
+// let soma = 0
+
+// while(numeroUsuario !== 0) {
+//     numeroUsuario = Number(prompt("Insira o nome do usuario"))
+//     console.log(numeroUsuario)
+//     soma += numeroUsuario
+// }
+// console.log(soma)
+
+
+// let numeroFor = 10
+// 	for(let i = 0; i <= numeroFor; i++){
+// 	      console.log(i)
+// }
+
+// const array = [1,2,3,4,5,6,7]
+
+// for (let i = 0; i<array.length; i++){
+//     const numero = array[i]
+//     console.log("o numero do indice ${i} e ${numero}")
+// }
+
+
+// let tipoUsuario = prompt("Digite o nome de usuario.").toUpperCase()
+// while (tipoUsuario !== "A"){
+//     console.log("Acesso negado")
+//     tipoUsuario = prompt("Digite o nome de usuario.").toUpperCase()
+// }
+// console.log("Bem vindo, Admin!!!")
+
+
+// let Tabuada = 2
+// for(let i = 0; i<=10;i++){
+//     console.log("2 * ", i," = ", Tabuada * i)
+// }
+
+const arrayString = ["","","",""]
+
+for (let i=0; i<arrayString.length; i++){
+    console.log(arrayString[i],arrayString.toUppercase())
+}
